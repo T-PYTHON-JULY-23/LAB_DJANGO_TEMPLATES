a975c8d3d99c67577b5b10f4b6b1b89f4bd41c03
suha <suha.alrajhi01@gmail.com>
2023-08-20 11:27:34 +0300

    suha alrajhi LabDjangoTamplets

--- LAB_DJANGO_TEMPLATES/website/urls.py
+++ LAB_DJANGO_TEMPLATES/website/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+app_name='website'
 urlpatterns = [
     path('today/', views.today_view, name='today'),
     path('random/password/', views.random_password_view, name='random_password'),
--- LAB_DJANGO_TEMPLATES/website/views.py
+++ LAB_DJANGO_TEMPLATES/website/views.py
@@ -2,20 +2,20 @@
 import datetime
 import random
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse ,HttpRequest
 
-def today_view(request):
+def today_view(request:HttpRequest):
     today = datetime.date.today()
     return render(request, 'website/today.html', {'today': today})
 
 
-def random_password_view(request):
+def random_password_view(request:HttpRequest):
     characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()'
     length = 12
     random_password = ''.join(random.choice(characters) for _ in range(length))
     return render(request, 'website/random_password.html', {'random_password': random_password})
 
-def favorite_games_view(request):
+def favorite_games_view(request:HttpRequest):
     favorite_games = ['Game 1', 'Game 2', 'Game 3', 'Game 4', 'Game 5']
     return render(request, 'website/favorite_games.html', {'favorite_games': favorite_games})
 
